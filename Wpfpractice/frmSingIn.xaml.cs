40de2d6d64be2f0cec811cd8c25487cc28f29cf2
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-10-21 00:24:02 -0500

    add singin

--- Wpfpractice/frmSingIn.xaml.cs
+++ Wpfpractice/frmSingIn.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows;
 
 namespace Wpfpractice;
@@ -7,5 +8,22 @@ public partial class frmSingIn : Window
     public frmSingIn()
     {
         InitializeComponent();
+    }
+
+    private void FrmSingIn_OnClosed(object? sender, EventArgs e)
+    {
+        if (MessageBox.Show("Are you sure to exit ?",
+                "Hola",
+                MessageBoxButton.OKCancel,
+                MessageBoxImage.Question,
+                MessageBoxResult.Cancel) == MessageBoxResult.Cancel)
+        {
+            Application.Current.Shutdown ();
+        }
+        else
+        {
+            this.Close();
+        }
+        
     }
 }
